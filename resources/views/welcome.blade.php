84b38e44f721b707894a387855f7338d0d33fbc9
dipeshs13 <dipeshshresthaa00@gmail.com>
2024-11-24 01:24:35 +0545

    footer updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,75 +94,161 @@
 
     <span class="text-2xl font-bold ml-48 ">Shop Air Force 1</span>
     <section class="text-gray-600 body-font">
-      <div class="container px-5 py-20 mx-auto">
-        <div class="flex flex-wrap -m-4">
-          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a class="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="{{ asset('images/shoes2.jpeg') }}">
-            </a>
-            <div class="mt-4">
-              {{-- <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3> --}}
-              <h2 class="text-gray-900 title-font text-lg font-medium">Air Jordan 3 Retro 'Cement Grey'</h2>
-              <p class="mt-1">Rs 8000</p>
+        <div class="container px-5 py-20 mx-auto">
+            <div class="flex flex-wrap -m-4">
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                        <img alt="ecommerce" class="object-cover object-center w-full h-full block"
+                            src="{{ asset('images/shoes2.jpeg') }}">
+                    </a>
+                    <div class="mt-4">
+                        {{-- <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3> --}}
+                        <h2 class="text-gray-900 title-font text-lg font-medium">Air Jordan 3 Retro 'Cement Grey'</h2>
+                        <p class="mt-1">Rs 8000</p>
+                    </div>
+                    <div>
+                        <button
+                            class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Buy
+                            Now</button>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                        <img alt="ecommerce" class="object-cover object-center w-full h-full block"
+                            src="{{ asset('images/shoes3.jpeg') }}">
+                    </a>
+                    <div class="mt-4">
+                        {{-- <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3> --}}
+                        <h2 class="text-gray-900 title-font text-lg font-medium">Air Jordan Retro 1 High OG</h2>
+                        <p class="mt-1">Rs 6000</p>
+                    </div>
+                    <div>
+                        <button
+                            class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Buy
+                            Now</button>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                        <img alt="ecommerce" class="object-cover object-center w-full h-full block"
+                            src="{{ asset('images/shoes4.jpeg') }}">
+                    </a>
+                    <div class="mt-4">
+                        {{-- <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3> --}}
+                        <h2 class="text-gray-900 title-font text-lg font-medium">Jordan 1 Retro White Black CDP</h2>
+                        <p class="mt-1">Rs 5000</p>
+                    </div>
+                    <div>
+                        <button
+                            class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Buy
+                            Now</button>
+                    </div>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                        <img alt="ecommerce" class="object-cover object-center w-full h-full block"
+                            src="{{ asset('images/shoes5.jpeg') }}">
+                    </a>
+                    <div class="mt-4">
+                        {{-- <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3> --}}
+                        <h2 class="text-gray-900 title-font text-lg font-medium">Jordan 1 Mid RM</h2>
+                        <p class="mt-1">Rs 4500</p>
+                    </div>
+                    <div>
+                        <button
+                            class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Buy
+                            Now</button>
+                    </div>
+                </div>
             </div>
-            <div>
-              <button
-              class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Buy
-              Now</button>
-            </div>
-          </div>
-          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a class="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="{{ asset('images/shoes3.jpeg') }}">
-            </a>
-            <div class="mt-4">
-              {{-- <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3> --}}
-              <h2 class="text-gray-900 title-font text-lg font-medium">Air Jordan Retro 1 High OG</h2>
-              <p class="mt-1">Rs 6000</p>
-            </div>
-            <div>
-              <button
-              class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Buy
-              Now</button>
-            </div>
-          </div>
-          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a class="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="{{ asset('images/shoes4.jpeg') }}">
-            </a>
-            <div class="mt-4">
-              {{-- <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3> --}}
-              <h2 class="text-gray-900 title-font text-lg font-medium">Jordan 1 Retro White Black CDP</h2>
-              <p class="mt-1">Rs 5000</p>
-            </div>
-            <div>
-              <button
-              class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Buy
-              Now</button>
-            </div>
-          </div>
-          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a class="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="{{ asset('images/shoes5.jpeg') }}">
-            </a>
-            <div class="mt-4">
-              {{-- <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3> --}}
-              <h2 class="text-gray-900 title-font text-lg font-medium">Jordan 1 Mid RM</h2>
-              <p class="mt-1">Rs 4500</p>
-            </div>
-            <div>
-              <button
-              class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Buy
-              Now</button>
-            </div>
-          </div>
-          </div>
         </div>
-      </div>
+        </div>
     </section>
-<footer class="h-64">
 
-</footer>
+    <footer class="text-gray-600 body-font border-t-2 border-gray-500">
+        <div
+            class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+
+            <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+                <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Resources</h2>
+                    <nav class="list-none mb-10">
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800">Gift Cards</a>
+                        </li>
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800">Find a Store</a>
+                        </li>
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800">Membership</a>
+                        </li>
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800">Nike Journal</a>
+                        </li>
+                    </nav>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Help</h2>
+                    <nav class="list-none mb-10">
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800"> Get Help
+                            </a>
+                        </li>
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800"> Order Status
+                            </a>
+                        </li>
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800"> Returns</a>
+                        </li>
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800"> Order Cancellation</a>
+                        </li>
+                    </nav>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Promotions & Discounts
+
+                    </h2>
+                    <nav class="list-none mb-10">
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800"> Student
+                            </a>
+                        </li>
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800">Military</a>
+                        </li>
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800">Teacher</a>
+                        </li>
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800"> First Responders & Medical Professionals</a>
+                        </li>
+                    </nav>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Company</h2>
+                    <nav class="list-none mb-10">
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800"> About Nike
+                            </a>
+                        </li>
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800"> News
+                            </a>
+                        </li>
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800">Careers</a>
+                        </li>
+                        <li>
+                            <a class="text-gray-600 hover:text-gray-800">Investors</a>
+                        </li>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+    </footer>
 </body>
 
 </html>
